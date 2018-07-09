61cf84d0cac2c85eaba2af93197130cbd2e4bbb2
Luiz Honorato <lhehonorato@gmail.com>
2018-07-09 12:17:56 -0300

    ToDo App

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -6,10 +6,17 @@ export default props => (
         <PageHeader name='Sobre' small='o app'></PageHeader>
 
         <h2>Finalidade</h2>
+            <p>Esse aplicativo tem a finalidade de cadastrar e gerenciar tarefas.</p>
         
         <h2>Sobre o build</h2>
-        
+            <p>O app foi criado com a utilização das seguintes ferramentas:</p>
+            <ul>
+                <li>Bootstrap</li>
+                <li>React</li>
+                <li>Redux</li>
+                <li>Webpack</li>
+            </ul>
         <h2>Versão</h2>
-        
+            <p>1.0</p>
     </div>
 )
